8dad1e8bbc5343aba12705ab61307ace9fb2c4f7
胡 <func-dev-20@signify.com>
2023-10-10 13:35:26 +0800

    SwiftUI

---
+++ SwiftUIDemo/SwiftUIDemo/01Text/HAttributedString.swift
@@ -0,0 +1,23 @@
+//
+//  HAttributedString.swift
+//  SwiftUIDemo
+//
+//  Created by 胡某人 on 2023/10/10.
+//
+
+import SwiftUI
+
+struct HAttributedString: View {
+    var body: some View {
+        List {
+            // attributedString
+            // SwiftUI de text 视图可以渲染使用Foundation的AttributedString
+        }
+    }
+}
+
+struct HAttributedString_Previews: PreviewProvider {
+    static var previews: some View {
+        HAttributedString()
+    }
+}
